a86331091854eb4b68646ee9827e4d87e1c35ef0
Patrick Thibaudeau <thibaud@yorku.yorku.ca>
2017-06-12 09:12:21 -0400

    RUpdated config.js to now use production server

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 //This file contains an array that can then be used globally througout all JS functions and files within this project
 var config = {
-    webServiceUrl:'http://patdev.glendon.yorku.ca/moodle/webservice/rest/server.php?',
-    webServiceToken:'3f91ea6afcc860d63ba2f0a8915412d0',
+    webServiceUrl:'http://admin.glendon.yorku.ca/moodle/webservice/rest/server.php?',
+    webServiceToken:'c6edf616d1c5ecc323172c813c21d990',
 }
 
