cf328a80ad3ca9af95b0f46520dd80b856f7f42c
Patrick Thibaudeau <thibaud@yorku.yorku.ca>
2017-08-18 09:52:39 -0400

    Https enabled

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 //This file contains an array that can then be used globally througout all JS functions and files within this project
 var config = {
-    webServiceUrl:'http://admin.glendon.yorku.ca/webservice/rest/server.php?', //production
+    webServiceUrl:'https://admin.glendon.yorku.ca/webservice/rest/server.php?', //production
     webServiceToken:'c6edf616d1c5ecc323172c813c21d990', //production
     //webServiceUrl:'http://patdev.glendon.yorku.ca/moodle/webservice/rest/server.php?', //dev
     //webServiceToken:'3f91ea6afcc860d63ba2f0a8915412d0', //dev
